2c0cd584871c6332a986443b35e8ce40093a1436
Liza Romaniuk <elizaveta.romaniuk@gmail.com>
2022-08-24 18:33:18 +0300

    updates

--- script.js
+++ script.js
@@ -14,10 +14,7 @@ function enter(e){
 
 async function getInfo(data){
     const res = await fetch(`${api.endpoint}weather?q=${data}&units=metric&appID=${api.key}`);
-    // const res = await fetch(`${api.endpoint}weather?q=${data}&appid=${api.key}`);
     const result = await res.json();
-    console.dir(res);
-    console.dir(result);
     displayResult(result);
 }
 
@@ -40,7 +37,6 @@ function displayResult(result){
     let dataFeelsLike = result.main.feels_like;
     let dataHumidity = result.main.humidity;
     let dataPressure = result.main.pressure;
-    console.log(dataPressure);
 
     feelsLike.innerHTML = `${Math.round(dataFeelsLike)} °`;
     humidity.innerHTML = `${dataHumidity} %`;
@@ -49,14 +45,74 @@ function displayResult(result){
     let description = document.querySelector('.weather_description');
     description.innerText = dataDescription;
 
-    console.log(data);
+    changeImage(dataDescription);
 
-    // let feelsLike = document.querySelector('#feelslike');
-    // feelsLike.textContent = `Feels like: ${Math.round(result.main.feels_like)} °`;
+    console.log(dataDescription)
 
-    // let conditions = document.querySelector('#conditions');
-    // conditions.textContent = `${result.weather[0].main}`;
-
-    // let varation = document.querySelector('#varation');
-    // varation.textContent = `Min: ${Math.round(result.main.temp_min)}° Max: ${Math.round(result.main.temp_max)}°`
 }
+
+
+async function showConstantData(){
+    const res = await fetch(`${api.endpoint}weather?q=kyiv&units=metric&appID=${api.key}`);
+    const result = await res.json();
+    // console.log(result);
+
+    let city = document.querySelector('.city_name');
+    city.textContent = `${result.name}, ${result.sys.country}`;
+
+    let temperature = document.querySelector('.temperature');
+    temperature.textContent = `${Math.round(result.main.temp)}`;
+
+    let feelsLike = document.querySelector('.feels_like');
+    let humidity = document.querySelector('.humidity');
+    let pressure = document.querySelector('.pressure');
+
+    let image = document.querySelector('.img');
+    let data = result.weather[0];
+    let dataDescription = result.weather[0].description;
+    let dataFeelsLike = result.main.feels_like;
+    let dataHumidity = result.main.humidity;
+    let dataPressure = result.main.pressure;
+    // console.log(dataPressure);
+
+    feelsLike.innerHTML = `${Math.round(dataFeelsLike)} °`;
+    humidity.innerHTML = `${dataHumidity} %`;
+    pressure.innerHTML = `${dataPressure} hPa`;
+
+    let description = document.querySelector('.weather_description');
+    description.innerText = dataDescription;
+
+    changeImage(dataDescription);
+
+}
+showConstantData();
+
+
+function changeImage(descr){
+    let backgroundImg = document.querySelector('.content');
+
+   
+    
+    if(descr === 'clear sky'){
+        backgroundImg.style.backgroundImage = "url('https://images.unsplash.com/photo-1623598019453-81fcbc1aaae8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80')"
+
+        console.log('test')
+    } else if(descr === 'few clouds') {
+        console.log('few clouds');
+        backgroundImg.style.backgroundImage = "url('https://images.unsplash.com/photo-1601297183305-6df142704ea2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80')"
+        
+    } else if(descr === 'overcast clouds') {
+        console.log('overcast clouds');
+        backgroundImg.style.backgroundImage = "url('https://images.unsplash.com/photo-1485249245068-d8dc50b77cc7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2231&q=80')"
+
+    } else if(descr === 'scattered clouds') {
+        backgroundImg.style.backgroundImage = "url('https://images.unsplash.com/photo-1642447733831-2cdd9f5efae7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2188&q=80')"
+    } else if(descr === 'haze') {
+        backgroundImg.style.backgroundImage = "url('https://images.unsplash.com/36/STzPBJUsSza3mzUxiplj_DSC09775.JPG?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1761&q=80')"
+    } else if(descr === 'mist') {
+        backgroundImg.style.backgroundImage = "url('https://images.unsplash.com/photo-1543968996-ee822b8176ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2128&q=80')"
+    }
+
+}
+
+// backgroundImg.style.backgroundImage = "url('')"
