2a65ae59e01d5991458f539454fc327436656ecf
Liza Romaniuk <elizaveta.romaniuk@gmail.com>
2022-08-24 22:50:52 +0300

    some night pictures

--- script.js
+++ script.js
@@ -117,6 +117,39 @@ function changeImage(descr){
         backgroundImg.style.backgroundImage = "url('https://images.unsplash.com/photo-1528485238486-507af7c0aa19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2232&q=80')";
     }
 
+    if(descr === 'overcast clouds' && time >=8 && time <= 17) {
+        console.log('overcast clouds');
+        backgroundImg.style.backgroundImage = "url('https://images.unsplash.com/photo-1485249245068-d8dc50b77cc7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2231&q=8')"
+    } else if (descr === 'overcast clouds'){
+        console.log('overcast clouds');
+        backgroundImg.style.backgroundImage = "url('https://images.unsplash.com/photo-1496089479256-16374dc12c9b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2144&q=80')";
+    }
+
+    if(descr === 'haze' && time >=8 && time <= 17) {
+        console.log('haze');
+        backgroundImg.style.backgroundImage = "url('https://images.unsplash.com/36/STzPBJUsSza3mzUxiplj_DSC09775.JPG?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1761&q=80')"
+    } else if (descr === 'haze'){
+        console.log('haze');
+        backgroundImg.style.backgroundImage = "url('https://images.unsplash.com/photo-1524392622596-5400234c9d95?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1776&q=80')";
+    }
+
+    if(descr === 'mist' && time >=8 && time <= 17) {
+        console.log('mist');
+        backgroundImg.style.backgroundImage = "url('https://images.unsplash.com/photo-1543968996-ee822b8176ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2128&q=80')"
+    } else if (descr === 'mist'){
+        console.log('mist');
+        backgroundImg.style.backgroundImage = "url('https://images.unsplash.com/photo-1494376877685-d3d2559d4f82?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80')";
+    }
+
+
+    if(descr === 'broken clouds' && time >=8 && time <= 17) {
+        console.log('broken clouds');
+        backgroundImg.style.backgroundImage = "url('https://images.unsplash.com/photo-1621859178739-c8dc945b556e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80')"
+    } else if (descr === 'broken clouds'){
+        console.log('broken clouds');
+        backgroundImg.style.backgroundImage = "url('https://images.unsplash.com/photo-1512936537265-78c94e715a5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1776&q=80')";
+    }
+
     //  else if(descr === 'overcast clouds') {
     //     console.log('overcast clouds');
     //     backgroundImg.style.backgroundImage = "url('https://images.unsplash.com/photo-1485249245068-d8dc50b77cc7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2231&q=80')"
